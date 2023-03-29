060d648b122607383c457a1b1a83532b077748f3
Halil ŞAHİN <halil.ibrahim.sahin@hotmail.com>
2023-03-30 00:00:47 +0300

    First Commit

---
+++ Invo.izin.Yönetim.Sistemi/Invo.izin.Yönetim.Sistemi.Web.MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Invo.izin.Yönetim.Sistemi.Web.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Invo.izin.Yönetim.Sistemi.Web.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Invo.izin.Yönetim.Sistemi/Invo.izin.Yönetim.Sistemi.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Invo.izin.Yönetim.Sistemi.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
